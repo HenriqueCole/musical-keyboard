d618180084e180632bd98124c2e105aca80ed8a3
HenriqueCole <henriquecolefernandes@gmail.com>
2022-09-08 09:26:11 -0300

    Finished

--- script.js
+++ script.js
@@ -1,130 +1,142 @@
 function C() {
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('C4', '8n')
-    document.addEventListener('keydown', function(event) {
-        if (event.code === 'KeyQ') {
-            synth.triggerAttackRelease('C4', '8n')
-        }
-    })
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyQ') {
+        C()
+    }
+})
+
 function D() {
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('D4', '8n')
-    document.addEventListener('keydown', function(event) {
-        if (event.code === 'KeyW') {
-            synth.triggerAttackRelease('D4', '8n')
-        }
-    })
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyW') {
+        D()
+    }
+})
+
 function E() {
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('E4', '8n')
-    document.addEventListener('keydown', function(event) {
-        if (event.code === 'KeyE') {
-            synth.triggerAttackRelease('E4', '8n')
-        }
-    })
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyE') {
+        E()
+    }
+})
+
 function F() {
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('F4', '8n')
-    document.addEventListener('keydown', function(event) {
-        if (event.code === 'KeyR') {
-            synth.triggerAttackRelease('F4', '8n')
-        }
-    })
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyR') {
+        F()
+    }
+})
+
 function G() {
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('G4', '8n')
-    document.addEventListener('keydown', function(event) {
-        if (event.code === 'KeyT') {
-            synth.triggerAttackRelease('G4', '8n')
-        }
-    })
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyT') {
+        G()
+    }
+})
+
 function A() {
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('A4', '8n')
-    document.addEventListener('keydown', function(event) {
-        if (event.code === 'KeyY') {
-            synth.triggerAttackRelease('A4', '8n')
-        }
-    })
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyY') {
+       A()
+    }
+})
+
 function B() {
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('B4', '8n')
-    document.addEventListener('keydown', function(event) {
-        if (event.code === 'KeyU') {
-            synth.triggerAttackRelease('B4', '8n')
-        }
-    })
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyU') {
+       B()
+    }
+})
+
 function C2() {
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('C5', '8n')
-    document.addEventListener('keydown', function(event) {
-        if (event.code === 'KeyI') {
-            synth.triggerAttackRelease('C5', '8n')
-        }
-    })
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyI') {
+        C2()
+    }
+})
 
 function D2() {
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('D5', '8n')
-    document.addEventListener('keydown', function(event) {
-        if (event.code === 'KeyO') {
-            synth.triggerAttackRelease('D5', '8n')
-        }
-    })
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyO') {
+        D2()
+    }
+})
+
 function E2() {
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('E5', '8n')
-    document.addEventListener('keydown', function(event) {
-        if (event.code === 'KeyP') {
-            synth.triggerAttackRelease('E5', '8n')
-        }
-    })
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyP') {
+        E2()
+    }
+})
+
 function F2() {
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('F5', '8n')
-    document.addEventListener('keydown', function(event) {
-        if (event.code === 'KeyZ') {
-            synth.triggerAttackRelease('F5', '8n')
-        }
-    })
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyZ') {
+        F2();
+    }
+})
+
 function G2() {
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('G5', '8n')
-    document.addEventListener('keydown', function(event) {
-        if (event.code === 'KeyX') {
-            synth.triggerAttackRelease('G5', '8n')
-        }
-    })
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyX') {
+        G2();
+    }
+})
+
 function A2() {
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('A5', '8n')
-    document.addEventListener('keydown', function(event) {
-        if (event.code === 'KeyD') {
-            synth.triggerAttackRelease('AC', '8n')
-        }
-    })
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyC') {
+        A2();
+    }
+})
+
 function B2() {
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('B5', '8n')
-    document.addEventListener('keydown', function(event) {
-        if (event.code === 'KeyV') {
-            synth.triggerAttackRelease('B5', '8n')
-        }
-    })
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyV') {
+        B2();
+    }
+})
 
 
 
@@ -134,45 +146,98 @@ function B2() {
 function CS(){
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('C#4', '8n')
-    document.addEventListener('keydown', function(event) {
-        if (event.code === 'Digit2') {
-            synth.triggerAttackRelease('C#4', '8n')
-        }
-    })
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Digit2') {
+        C2()
+    }
+})
+
 function DS(){
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('D#4', '8n')
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Digit3') {
+       DS()
+    }
+})
+
 function FS(){
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('F#4', '8n')
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Digit5') {
+        FS()
+    }
+})
 function GS(){
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('G#4', '8n')
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Digit6') {
+        GS()
+    }
+})
+
 function AS(){
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('A#4', '8n')
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Digit7') {
+       AS()
+    }
+})
+
 function CS2(){
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('C#5', '8n')
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Digit9') {
+       CS2()
+    }
+})
+
 function DS2(){
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('D#5', '8n')
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Digit0') {
+       DS2()
+    }
+})
+
 function FS2(){
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('F#5', '8n')
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyS') {
+       FS2()
+    }
+})
+
 function GS2(){
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('G#5', '8n')
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyD') {
+       GS2()
+    }
+})
+
 function AS2(){
     const synth = new Tone.Synth().toMaster()
     synth.triggerAttackRelease('A#5', '8n')
 }
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'KeyF') {
+       AS2()
+    }
+})
